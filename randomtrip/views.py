5f78f3bb9cd11ce17b9bd8059692c9165cc91a41
Amon3141 <kizawaamon@gmail.com>
2024-08-17 07:49:00 +0900

    Did url settings

--- randomtrip/views.py
+++ randomtrip/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+import json
 
 # Create your views here.
+def randomtrip(request):
+  return render(request, 'randomtrip.html')
