585c076bc4c7a72f6a525b95a56a08841663ef03
Hugo Aonuma <haonuma@students.cs.ubc.ca>
2024-08-17 10:08:28 -0700

    another update with the button

--- staticfiles/randomtrip/js/scripts.js
+++ staticfiles/randomtrip/js/scripts.js
@@ -3,3 +3,15 @@
 function goToOptionScreen() {
     window.location.href = "/option_screen/";
 }
+
+function beginFinding() {
+    var number = document.getElementById('numberInput').value;
+    if (number === "") {
+        alert("Please enter a number.");
+        return;
+    }
+
+    // Redirect or process the number as needed
+    alert("You entered: " + number);
+    // Example: window.location.href = "/some_other_page/?number=" + number;
+}
